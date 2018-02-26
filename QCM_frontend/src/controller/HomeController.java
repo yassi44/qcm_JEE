757eeef7604c3a02e3b34a5a62d4d158ca95d213
youKnow44 <yasscomoko@gmail.com>
2018-02-26 16:18:16 +0100

    YS- front vide

---
+++ QCM_frontend/src/controller/HomeController.java
@@ -0,0 +1,9 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "HomeController", urlPatterns = "/home")
+public class HomeController extends HttpServlet {
+
+}
